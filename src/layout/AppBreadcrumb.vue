2cdac3d4dcb2ba9a345bc46429d46af2e9fb66eb
ronaldidro <rjdiazi10@gmail.com>
2026-05-15 15:29:51 -0500

    update breadcrumb styles

--- src/layout/AppBreadcrumb.vue
+++ src/layout/AppBreadcrumb.vue
@@ -1,9 +1,9 @@
 <template>
-  <div class="breadcrumb-card">
+  <div class="mb-7 rounded-md">
     <Breadcrumb
       :home="{ icon: 'pi pi-home', to: '/' }"
       :model="items"
-      class="p-0! px"
+      class="p-0! bg-transparent!"
     />
   </div>
 </template>
@@ -11,17 +11,3 @@
 <script setup lang="ts">
 defineProps<{ items: { label: string }[] }>();
 </script>
-
-<style scoped>
-.breadcrumb-card {
-  background: var(--surface-card);
-  padding-inline: 2rem;
-  padding-block: 1rem;
-  margin-bottom: 2rem;
-  border-radius: var(--content-border-radius);
-
-  &:last-child {
-    margin-bottom: 0;
-  }
-}
-</style>
